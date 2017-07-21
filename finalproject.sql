2dfdd1baa4bcd94087faedb2c747f35f66aec1b9
ChrisScott9456 <christopherscott9456@gmail.com>
2017-07-21 17:30:40 -0400

    booking 2
    -Added sql file to create and populate tables
    -Added dropdown menus in menu.php to shown flight and rental car options from SQL database

---
+++ finalproject.sql
@@ -0,0 +1,75 @@
+DROP TABLE IF EXISTS `inventory`;
+
+CREATE TABLE IF NOT EXISTS `inventory` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `itype` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  primary key (id));
+  
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  primary key (id));
+  
+INSERT INTO customers (username, password) VALUES
+	('cscott71', 'test');
+
+
+INSERT INTO inventory (itype, name, price) VALUES
+	('flight', 'Alabama', '150'),
+	('flight', 'Alaska', '1000'),
+	('flight', 'Arizona', '700'),
+	('flight', 'Arkansas', '400'),
+	('flight', 'California', '800'),
+	('flight', 'Colorado', '450'),
+	('flight', 'Connecticut', '200'),
+	('flight', 'Delaware', '210'),
+	('flight', 'Florida', '125'),
+	('flight', 'Georgia', '0'),
+	('flight', 'Hawaii', '1100'),
+	('flight', 'Idaho', '525'),
+	('flight', 'Illinois', '210'),
+	('flight', 'Indiana', '250'),
+	('flight', 'Iowa', '220'),
+	('flight', 'Kansas', '275'),
+	('flight', 'Kentucky', '250'),
+	('flight', 'Louisiana', '150'),
+	('flight', 'Maine', '325'),
+	('flight', 'Maryland', '325'),
+	('flight', 'Massachusetts', '300'),
+	('flight', 'Michigan', '425'),
+	('flight', 'Minnesota', '350'),
+	('flight', 'Mississippi', '250'),
+	('flight', 'Missouri', '200'),
+	('flight', 'Montana', '300'),
+	('flight', 'Nebraska', '400'),
+	('flight', 'Nevada', '350'),
+	('flight', 'New Hampshire', '250'),
+	('flight', 'New Jersey', '250'),
+	('flight', 'New Mexico', '350'),
+	('flight', 'New York', '450'),
+	('flight', 'North Carolina', '125'),
+	('flight', 'North Dakota', '300'),
+	('flight', 'Ohio', '250'),
+	('flight', 'Oklahoma', '250'),
+	('flight', 'Oregon', '650'),
+	('flight', 'Pennsylvania', '350'),
+	('flight', 'Rhode Island', '250'),
+	('flight', 'South Carolina', '150'),
+	('flight', 'South Dakota', '375'),
+	('flight', 'Tennessee', '150'),
+	('flight', 'Texas', '500'),
+	('flight', 'Utah', '520'),
+	('flight', 'Vermont', '210'),
+	('flight', 'Virginia', '180'),
+	('flight', 'Washington', '500'),
+	('flight', 'West Virginia', '350'),
+	('flight', 'Wisconsin', '250'),
+	('flight', 'Wyoming', '350'),
+	('car', 'SUV', '300'),
+	('car', 'Compact', '150'),
+	('car', 'Midsize', '200'),
+	('car', 'Luxury', '500'),
+	('parking', 'Parking', '50');
--- menu.php
+++ menu.php
@@ -9,7 +9,47 @@
 </head>
 
 <body>
-  
+  <h1>Choose a Flight:</h1>
+
+  <?php
+  $conn = new mysqli('localhost', 'root', '', 'finalproject')
+  or die ('Cannot connect to db');
+
+      $result = $conn->query("select name from inventory where itype='flight'");
+
+      echo "<select name='flight'>";
+
+      while ($row = $result->fetch_assoc()) {
+
+                    unset($id, $name);
+                    $name = $row['name'];
+                    echo '<option>'.$name.'</option>';
+
+  }
+
+      echo "</select>";
+  ?>
+
+  <h1>Choose a Rental Car:</h1>
+
+  <?php
+  $conn = new mysqli('localhost', 'root', '', 'finalproject')
+  or die ('Cannot connect to db');
+
+      $result = $conn->query("select name from inventory where itype='car'");
+
+      echo "<select id='car'>";
+
+      while ($row = $result->fetch_assoc()) {
+
+                    unset($id, $name);
+                    $name = $row['name'];
+                    echo '<option>'.$name.'</option>';
+
+  }
+
+      echo "</select>";
+  ?>
 
 </body>
 
